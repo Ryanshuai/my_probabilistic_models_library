acadd063dbfd4babd7d1b49cec50e0b89a81c7d0
Shuai Yan <yanshuairyan@gmail.com>
2022-01-10 13:43:14 -0800

    .

--- tst_b.py
+++ tst_b.py
@@ -5,9 +5,9 @@ from randomvariable import RandomVariable, p
 L0 = RandomVariable([0.3333333, 0.3333333, 0.333333])
 C = RandomVariable([0.5, 0.5])
 
-mat = [[0.95, 0.05],
-       [0.10, 0.90],
-       [0.10, 0.90]]
+mat = [[0.10, 0.90],
+       [0.95, 0.05],
+       [0.95, 0.05]]
 SL0 = RandomVariable(mat, parents=(L0,))
 
 mat = [[0.95, 0.05],
@@ -24,9 +24,9 @@ mat = [[[1, 0, 0],
 
 L1 = RandomVariable(mat, parents=(L0, C))
 
-mat = [[0.95, 0.05],
-       [0.10, 0.90],
-       [0.10, 0.90]]
+mat = [[0.10, 0.90],
+       [0.95, 0.05],
+       [0.95, 0.05]]
 SL1 = RandomVariable(mat, parents=(L1,))
 
 mat = [[0.95, 0.05],
@@ -34,7 +34,8 @@ mat = [[0.95, 0.05],
        [0.95, 0.05]]
 SR1 = RandomVariable(mat, parents=(L1,))
 
-T = RandomVariable([0.2, 0.8])
+LT = RandomVariable([0.2, 0.8])
+RT = RandomVariable([0.2, 0.8])
 
 mat = [[[[1, 0],
          [1, 0]],
@@ -44,7 +45,7 @@ mat = [[[[1, 0],
          [0, 1]],
         [[1, 0],
          [0, 1]]]]
-RL = RandomVariable(mat, parents=(T, SL0, SL1))
+RL = RandomVariable(mat, parents=(LT, SL0, SL1))
 
 mat = [[[[1, 0],
          [1, 0]],
@@ -54,7 +55,7 @@ mat = [[[[1, 0],
          [0, 1]],
         [[1, 0],
          [0, 1]]]]
-RR = RandomVariable(mat, parents=(T, SR0, SR1))
+RR = RandomVariable(mat, parents=(RT, SR0, SR1))
 
 p1 = p(({C: 1, RR: 1, RL: 1}))
 print(p1)
